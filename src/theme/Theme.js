ab64504222d450ab32c90176e1a5c0111b95ad1d
Chamidi Perera <chaminihansa@gmail.com>
2024-03-02 22:08:36 +0530

    added images and the theme file

---
+++ src/theme/Theme.js
@@ -0,0 +1,29 @@
+export const colorTheme = {
+  primaryColor: '#025489',
+  secondaryColor: '#D4DEFF',
+  black: '#000000',
+  white: '#ffffff',
+};
+
+export const textStyles = {
+  title: {
+    color: '#000000',
+    fontSize: 20,
+    fontFamily: 'Lexend-Bold',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  description: {
+    color: '#000000',
+    fontSize: 14,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontFamily: 'Lexend-Regular',
+    width: '90%',
+  },
+  subtitle: {
+    color: '#000000',
+    fontSize: 16,
+    fontFamily: 'Lexend-Medium',
+  },
+};
